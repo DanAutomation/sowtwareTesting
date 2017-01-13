d34dd97ef547027c5e919a4ef183e9eea8a60751
DanAutomation <kasta-laga@yandex.ru>
2017-01-13 23:58:30 +0200

    created

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/FirstClass.java
@@ -0,0 +1,10 @@
+package ru.stqa.pft.addressbook;
+
+/**
+ * Created by User on 13.01.2017.
+ */
+public class FirstClass {
+    public static  void  main (String[] args){
+        System.out.print("Hello, World!");
+    }
+}
