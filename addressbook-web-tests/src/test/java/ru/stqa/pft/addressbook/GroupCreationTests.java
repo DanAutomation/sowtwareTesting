1158afa2039dbffd3979530e8a4bf351402fb7f0
DanAutomation <kasta-laga@yandex.ru>
2017-01-14 01:37:41 +0200

    Revert "refactoring of methods and parametrs"
    This reverts commit f0c37035ddc75a0039ed25ddf24d311f953e307c.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -4,10 +4,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
+import static org.testng.Assert.*;
 
 import java.util.concurrent.TimeUnit;
-
+import java.util.Date;
+import java.io.File;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
+import static org.openqa.selenium.OutputType.*;
 
 public class GroupCreationTests {
 
@@ -21,8 +27,8 @@ public class GroupCreationTests {
 		System.setProperty("webdriver.gecko.driver", driverPath+"geckodriver.exe");
 		driver = new FirefoxDriver();
      */
-    //   String path = "D://projects//softwareTesting//sowtwareTesting//sendBox//geckodriver//geckodriver.exe";
-    //   WebDriver wd;
+ //   String path = "D://projects//softwareTesting//sowtwareTesting//sendBox//geckodriver//geckodriver.exe";
+ //   WebDriver wd;
     WebDriver wd;
 
     @BeforeMethod
@@ -30,65 +36,41 @@ public class GroupCreationTests {
         System.setProperty("webdriver.gecko.driver", "D://softwareTesting//sowtwareTesting//addressbook-web-tests//src//browsers//geckodriver.exe");
         System.setProperty("webdriver.chrome.driver", "D://softwareTesting//sowtwareTesting//addressbook-web-tests//src//browsers//chromedriver.exe");
         wd = new ChromeDriver();
-        //       wd = new FirefoxDriver();
+ //       wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/group.php");
-        login("admin", "secret");
-    }
-
-    private void login(String username, String password) {
         wd.findElement(By.id("header")).click();
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys(username);
+        wd.findElement(By.name("user")).sendKeys("admin");
         wd.findElement(By.name("pass")).click();
         wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys(password);
+        wd.findElement(By.name("pass")).sendKeys("secret");
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
     }
-
+    
     @Test
     public void testGroupCreation() {
-        gotoGroupPage();
-        initGroupCreation();
-        fillGroupForm(new GroupData("test1", "header", "footer"));
-        submitGroupCreation();
-        returnToGroupPage();
-    }
-
-    private void returnToGroupPage() {
-        wd.findElement(By.linkText("group page")).click();
-    }
-
-    private void submitGroupCreation() {
-        wd.findElement(By.name("submit")).click();
-    }
-
-    private void fillGroupForm(GroupData groupData) {
+        wd.findElement(By.linkText("groups")).click();
+        wd.findElement(By.name("new")).click();
         wd.findElement(By.name("group_name")).click();
         wd.findElement(By.name("group_name")).clear();
-        wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
+        wd.findElement(By.name("group_name")).sendKeys("test1");
         wd.findElement(By.name("group_header")).click();
         wd.findElement(By.name("group_header")).clear();
-        wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
+        wd.findElement(By.name("group_header")).sendKeys("header");
         wd.findElement(By.name("group_footer")).click();
         wd.findElement(By.name("group_footer")).clear();
-        wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+        wd.findElement(By.name("group_footer")).sendKeys("footer");
+        wd.findElement(By.name("submit")).click();
+        wd.findElement(By.linkText("group page")).click();
     }
-
-    private void initGroupCreation() {
-        wd.findElement(By.name("new")).click();
-    }
-
-    private void gotoGroupPage() {
-        wd.findElement(By.linkText("groups")).click();
-    }
-
+    
     @AfterMethod
     public void tearDown() {
         wd.close();
     }
-
+    
     public static boolean isAlertPresent(ChromeDriver wd) {
         try {
             wd.switchTo().alert();
